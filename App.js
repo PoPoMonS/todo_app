cb32f4dec5e2bc038e017debe11c41f551d3ea58
PoPoMonS <S25N.GH5@gmail.com>
2022-06-10 23:01:16 +0900

    Code Challenge Self and 3
    - Add Menu named sehyun
    - Add Edit Function

--- App.js
+++ App.js
@@ -13,37 +13,47 @@ import {
 import { theme } from "./colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Fontisto } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
-const STORAGE_KEY_WORKING = "@working";
+const STORAGE_KEY_MENUTYPE = "@menuType";
 const STORAGE_KEY_TODO = "@toDos";
 
+const WORK = 0;
+const SEHYUN = 1;
+const TRAVEL = 2;
+
 export default function App() {
-	const [working, setWorking] = useState(true);
-	const [text, setText] = useState("");
+	const [menuType, setMenuType] = useState(WORK);
+	const [textInput, setTextInput] = useState("");
+	const [textEdit, setTextEdit] = useState("");
 	const [toDos, setToDos] = useState({});
 
 	useEffect(() => {
 		loadToDos();
-		loadWorking();
+		loadMenuType();
 	}, []);
 
 	const travel = () => {
-		setWorking(false);
-		saveWorking(false);
+		setMenuType(TRAVEL);
+		saveMenuType(TRAVEL);
 	};
 	const work = () => {
-		setWorking(true);
-		saveWorking(true);
+		setMenuType(WORK);
+		saveMenuType(WORK);
 	};
-	const saveWorking = async (toSave) => {
-		await AsyncStorage.setItem(STORAGE_KEY_WORKING, JSON.stringify(toSave));
+	const sehyun = () => {
+		setMenuType(SEHYUN);
+		saveMenuType(SEHYUN);
 	};
-	const loadWorking = async () => {
-		const s = await AsyncStorage.getItem(STORAGE_KEY_WORKING);
-		setWorking(JSON.parse(s));
+	const saveMenuType = async (toSave) => {
+		await AsyncStorage.setItem(STORAGE_KEY_MENUTYPE, JSON.stringify(toSave));
 	};
-	const onChangeText = (payload) => {
-		setText(payload);
+	const loadMenuType = async () => {
+		const s = await AsyncStorage.getItem(STORAGE_KEY_MENUTYPE);
+		setMenuType(JSON.parse(s));
+	};
+	const onChangeTextInput = (payload) => {
+		setTextInput(payload);
 	};
 	const saveToDos = async (toSave) => {
 		await AsyncStorage.setItem(STORAGE_KEY_TODO, JSON.stringify(toSave));
@@ -56,16 +66,19 @@ export default function App() {
 		setToDos(JSON.parse(s));
 	};
 	const addToDo = async () => {
-		if (text === "") {
+		if (textInput === "") {
 			return;
 		}
 		// const newToDos = Object.assign({}, toDos, {
 		// 	[Date.now()]: { text, work: working },
 		// });
-		const newToDos = { ...toDos, [Date.now()]: { text, working, done: false } };
+		const newToDos = {
+			...toDos,
+			[Date.now()]: { textInput, menuType, done: false, editing: false },
+		};
 		setToDos(newToDos);
 		await saveToDos(newToDos);
-		setText("");
+		setTextInput("");
 	};
 	const deleteToDo = (key) => {
 		Alert.alert("Delete", "Sure?", [
@@ -92,6 +105,24 @@ export default function App() {
 		setToDos(newToDos);
 		await saveToDos(newToDos);
 	};
+	const editToDo = async (key) => {
+		const newToDos = { ...toDos };
+		if (newToDos[key].editing === true) {
+			newToDos[key].editing = false;
+			newToDos[key].textInput = textEdit;
+			setToDos(newToDos);
+			setTextEdit("");
+		} else {
+			newToDos[key].editing = true;
+			setTextEdit(newToDos[key].textInput);
+		}
+		setToDos(newToDos);
+		await saveToDos(newToDos);
+	};
+	const onChangeTextEdit = (payload) => {
+		setTextEdit(payload);
+	};
+	console.log(toDos);
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
@@ -100,17 +131,27 @@ export default function App() {
 					<Text
 						style={{
 							...styles.btnText,
-							color: working ? "ivory" : theme.grey,
+							color: menuType === WORK ? "ivory" : theme.grey,
 						}}
 					>
 						Work
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={sehyun}>
+					<Text
+						style={{
+							...styles.btnText,
+							color: menuType === SEHYUN ? "ivory" : theme.grey,
+						}}
+					>
+						세현
 					</Text>
 				</TouchableOpacity>
 				<TouchableOpacity onPress={travel}>
 					<Text
 						style={{
 							...styles.btnText,
-							color: !working ? "ivory" : theme.grey,
+							color: menuType === TRAVEL ? "ivory" : theme.grey,
 						}}
 					>
 						Travel
@@ -118,25 +159,50 @@ export default function App() {
 				</TouchableOpacity>
 			</View>
 			<TextInput
-				onChangeText={onChangeText}
+				onChangeText={onChangeTextInput}
 				onSubmitEditing={addToDo}
 				returnKeyType="done"
-				value={text}
-				placeholder={working ? "Add what to do" : "Where do you want to go"}
+				value={textInput}
+				placeholder={
+					menuType === WORK
+						? "Add what to do"
+						: menuType === SEHYUN
+						? "Hi Sehyun!!"
+						: "Where do you want to go"
+				}
 				style={styles.input}
 			></TextInput>
 			<ScrollView>
 				{Object.keys(toDos).map((key) =>
-					toDos[key].working === working ? (
+					toDos[key].menuType === menuType ? (
 						<View style={styles.toDo} key={key}>
-							<Text
-								style={{
-									...styles.toDoText,
-									textDecorationLine: toDos[key].done ? "line-through" : "none",
-								}}
-							>
-								{toDos[key].text}
-							</Text>
+							{toDos[key].editing ? (
+								<TextInput
+									onChangeText={onChangeTextEdit}
+									returnKeyType="done"
+									value={textEdit}
+									placeholder={
+										menuType === WORK
+											? "Add what to do"
+											: menuType === SEHYUN
+											? "Hi Sehyun!!"
+											: "Where do you want to go"
+									}
+									style={styles.toDoInput}
+								></TextInput>
+							) : (
+								<Text
+									style={{
+										...styles.toDoText,
+										textDecorationLine: toDos[key].done
+											? "line-through"
+											: "none",
+										color: toDos[key].done ? "grey" : "ivory",
+									}}
+								>
+									{toDos[key].textInput}
+								</Text>
+							)}
 							<View
 								style={{
 									flexDirection: "row",
@@ -146,10 +212,22 @@ export default function App() {
 								<TouchableOpacity
 									style={{ marginHorizontal: 5 }}
 									onPress={() => {
+										editToDo(key);
+									}}
+								>
+									<Entypo name="edit" size={22} color="ivory" />
+								</TouchableOpacity>
+								<TouchableOpacity
+									style={{ marginHorizontal: 5 }}
+									onPress={() => {
 										doneToDo(key);
 									}}
 								>
-									<Fontisto name="checkbox-active" size={18} color="ivory" />
+									{toDos[key].done ? (
+										<Fontisto name="toggle-on" size={24} color="ivory" />
+									) : (
+										<Fontisto name="toggle-off" size={24} color="ivory" />
+									)}
 								</TouchableOpacity>
 								<TouchableOpacity
 									style={{ marginHorizontal: 5 }}
@@ -204,5 +282,11 @@ const styles = StyleSheet.create({
 	toDoText: {
 		color: "ivory",
 		fontSize: 20,
+	},
+	toDoInput: {
+		backgroundColor: "ivory",
+		fontSize: 20,
+		// paddingHorizontal: 5,
+		// paddingVertical: 5,
 	},
 });
